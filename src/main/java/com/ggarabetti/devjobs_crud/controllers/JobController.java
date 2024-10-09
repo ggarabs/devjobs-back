59227e8332c514d3ab9e085fabae19a36cd4d7b9
gustavo <ggarabetti0607@gmail.com>
2024-10-08 21:32:37 -0300

    feat: basic CORS permissions added on CRUD

--- src/main/java/com/ggarabetti/devjobs_crud/controllers/JobController.java
+++ src/main/java/com/ggarabetti/devjobs_crud/controllers/JobController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,18 +30,21 @@ public class JobController {
     @Autowired
     private JobService jobService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public ResponseEntity getAllJobs() {
         List<Job> allJobs = jobService.getAllJobs();
         return ResponseEntity.ok(allJobs);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public ResponseEntity registerJob(@RequestBody @Valid JobRequestDTO data) {
         Job job = jobService.registerJob(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(job);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PutMapping()
     @Transactional
     public ResponseEntity updateJob(@RequestBody @Valid JobRequestDTO data) {
